67a54ac7ad2b2601e1c9ef0bd3d781b3fb01a534
Gabriel-by34 <museradotcom04@gmail.com>
2026-02-08 10:15:44 +0300

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Assigment.python1
+# python-assignment
--- lesson1assignment.py
+++ lesson1assignment.py
@@ -3,11 +3,21 @@
 #BMI=(weight)/Heightsquared
 #weight=78kgs
 #height=1.75
-BMI =(Weight  kg)/(Heights-quared)
-BMI=78/(1.75**2)
-print(BMI)
-
+weight=78
+height=1.75
+BMI=(weight)/(height**2)
+print("The BMI is;",BMI)
 
 # 2. Find the Area and perimeter of a rectangle whose length is 48cm and width is 25cm
+length=48
+width=25
+area=length*width
+print("The area of the rectangle is;",area)
+
+#Perimeters
+length=48
+width=25
+perimeter=(length+width)*2
+print("The perimeter of the rectangle is;",perimeter)
 
 # Research on python list, tuple and Dictionary Data types.
